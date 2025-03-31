b33a0c6d2eceff7ee2aac36b4ae248deb7c085c6
vxlhat <n11eversky@gmail.com>
2025-03-31 22:00:37 +0500

    real primary key

--- src/Storages/Mongo/DataModel.cs
+++ src/Storages/Mongo/DataModel.cs
@@ -6,9 +6,6 @@ namespace Amethyst.Storages.Mongo;
 public abstract class DataModel(string name)
 {
     [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
-
     public string Name { get; set; } = name;
 
     public abstract void Save();
