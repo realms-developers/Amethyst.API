9fa2de8881a8122d17ea166e6e4f3a72add46e48
dsaf <ghostyn678+git@gmail.com>
2025-04-16 03:28:42 -0300

    feat: ClearDropped()

--- src/World/WorldUtils.cs
+++ src/World/WorldUtils.cs
@@ -1,11 +1,24 @@
 ﻿using Amethyst.Network;
 using Amethyst.Players;
 using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
 
 namespace Amethyst.World;
 
 public static class WorldUtils
 {
+    public static void ClearDropped()
+    {
+        for (int i = 0; i < Main.item.Length; i++)
+        {
+            Main.item[i] = new();
+
+            NetMessage.SendData(21, -1, -1, NetworkText.Empty, i);
+            NetMessage.SendData(22, -1, -1, NetworkText.Empty, i, 255);
+        }
+    }
+
     public static void SendCombatText(float x, float y, string text, Color color, NetPlayer? target = null)
     {
         using PacketWriter writer = new();
