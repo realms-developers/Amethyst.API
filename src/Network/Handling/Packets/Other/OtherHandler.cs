d59f5862bbe34e217601e2d3cb59af7797f9e571
vxlhat <n11eversky@gmail.com>
2025-06-12 23:25:29 +0500

    impl: emote bubbles

--- src/Network/Handling/Packets/Other/OtherHandler.cs
+++ src/Network/Handling/Packets/Other/OtherHandler.cs
@@ -5,6 +5,7 @@ using Amethyst.Network.Structures;
 using Amethyst.Server.Entities;
 using Amethyst.Server.Entities.Players;
 using Terraria;
+using Terraria.GameContent.UI;
 
 namespace Amethyst.Network.Handling.Packets.Other;
 
@@ -16,6 +17,21 @@ public sealed class OtherHandler : INetworkHandler
     {
         NetworkManager.SetMainHandler<PlayerOrEntityTeleport>(OnPlayerOrEntityTeleport);
         NetworkManager.SetMainHandler<GolfPutBallInCup>(OnGolfPutBallInCup);
+        NetworkManager.SetMainHandler<VisualCreateEmoteBubble>(OnVisualCreateEmoteBubble);
+    }
+
+    private void OnVisualCreateEmoteBubble(PlayerEntity plr, ref VisualCreateEmoteBubble packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
+    {
+        if (plr.Phase != ConnectionPhase.Connected)
+        {
+            return;
+        }
+
+        if (packet.EmoteID >= 0 && packet.EmoteID < EmoteID.Count)
+        {
+            EmoteBubble.NewBubble(packet.EmoteID, new WorldUIAnchor(plr.TPlayer), 360);
+            EmoteBubble.CheckForNPCsToReactToEmoteBubble(packet.EmoteID, plr.TPlayer);
+        }
     }
 
     private void OnGolfPutBallInCup(PlayerEntity plr, ref GolfPutBallInCup packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
@@ -75,5 +91,6 @@ public sealed class OtherHandler : INetworkHandler
     {
         NetworkManager.SetMainHandler<PlayerOrEntityTeleport>(null);
         NetworkManager.SetMainHandler<GolfPutBallInCup>(null);
+        NetworkManager.SetMainHandler<VisualCreateEmoteBubble>(null);
     }
 }
