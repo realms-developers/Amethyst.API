5647d58f4cb3fb51df885fef37ee8a93f7d515d4
vxlhat <n11eversky@gmail.com>
2025-06-12 21:24:23 +0500

    impl: world nethandler

--- src/Network/Handling/Packets/World/WorldHandler.cs
+++ src/Network/Handling/Packets/World/WorldHandler.cs
@@ -1,8 +1,11 @@
 using Amethyst.Network.Handling.Base;
 using Amethyst.Network.Handling.Packets.Handshake;
 using Amethyst.Network.Packets;
+using Amethyst.Network.Structures;
 using Amethyst.Server.Entities.Players;
 using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.ID;
 using Terraria.Localization;
 
 namespace Amethyst.Network.Handling.Packets.World;
@@ -30,7 +33,7 @@ public sealed class WorldHandler : INetworkHandler
         if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
 
-        int above = ((packet.PlayerAbove != 0) ? 1 : (-1));
+        int above = (packet.PlayerAbove != 0) ? 1 : -1;
         switch (packet.Action)
         {
             case 0:
@@ -62,43 +65,248 @@ public sealed class WorldHandler : INetworkHandler
     {
         if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
+
+        if (Main.tile[packet.TileX, packet.TileY] == null)
+        {
+            Main.tile[packet.TileX, packet.TileY] = new Tile();
+        }
+
+        lock (Main.tile[packet.TileX, packet.TileY])
+        {
+            Main.tile[packet.TileX, packet.TileY].liquid = packet.LiquidCount;
+            Main.tile[packet.TileX, packet.TileY].liquidType(packet.LiquidType);
+
+            WorldGen.SquareTileFrame(packet.TileX, packet.TileY);
+            if (packet.LiquidCount == 0)
+            {
+                NetMessage.SendData(48, -1, plr.Index, NetworkText.Empty, packet.TileX, packet.TileY);
+            }
+        }
     }
 
     private void OnWorldWiringHitSwitch(PlayerEntity plr, ref WorldWiringHitSwitch packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
     {
         if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
+
+        Wiring.SetCurrentUser(plr.Index);
+		Wiring.HitSwitch(packet.TileX, packet.TileY);
+		Wiring.SetCurrentUser();
+
+		NetMessage.TrySendData(59, -1, plr.Index, NetworkText.Empty, packet.TileX, packet.TileY);
     }
 
     private void OnWorldToggleGemLock(PlayerEntity plr, ref WorldToggleGemLock packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
     {
         if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
+
+        WorldGen.ToggleGemLock(packet.TileX, packet.TileY, packet.IsOn);
     }
 
-    private void OnWorldTileRectangle(PlayerEntity plr, ref WorldTileRectangle packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
+    private unsafe void OnWorldTileRectangle(PlayerEntity plr, ref WorldTileRectangle packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
     {
         if (plr.Phase != ConnectionPhase.Connected || packet.StartX <= 5 || packet.StartY <= 5 || packet.StartX >= Main.maxTilesX - 5 || packet.StartY >= Main.maxTilesY - 5 ||
             packet.StartX + packet.SizeX <= 5 || packet.StartY + packet.SizeY <= 5 || packet.StartX + packet.SizeX >= Main.maxTilesX - 5 || packet.StartY + packet.SizeY >= Main.maxTilesY - 5)
             return;
+
+        if (packet.SizeX > 5 || packet.SizeY > 5)
+        {
+            ignore = true;
+            return;
+        }
+
+        for (int x = packet.StartX; x < packet.StartX + packet.SizeX; x++)
+        {
+            for (int y = packet.StartY; y < packet.StartY + packet.SizeY; y++)
+            {
+                if (Main.tile[x, y] == null)
+                {
+                    Main.tile[x, y] = new Tile();
+                }
+                var netTile = packet.Tiles[x, y];
+                var tilePtr = Main.tile[x, y].ptr;
+
+                tilePtr->active(netTile.Active);
+                tilePtr->type = netTile.TileID;
+                tilePtr->wall = netTile.Wall;
+                tilePtr->liquid = netTile.Liquid;
+                tilePtr->liquidType(netTile.LiquidID);
+                tilePtr->wire(netTile.Wire);
+                tilePtr->wire2(netTile.Wire2);
+                tilePtr->wire3(netTile.Wire3);
+                tilePtr->wire4(netTile.Wire4);
+                tilePtr->halfBrick(netTile.HalfBrick);
+                tilePtr->actuator(netTile.Actuator);
+                tilePtr->inActive(netTile.Inactive);
+                tilePtr->slope(netTile.Slope);
+                tilePtr->color(netTile.TileColor);
+                tilePtr->wallColor(netTile.WallColor);
+                tilePtr->fullbrightBlock(netTile.Fullbright);
+                tilePtr->fullbrightWall(netTile.FullbrightWall);
+                tilePtr->invisibleBlock(netTile.Invisible);
+                tilePtr->invisibleWall(netTile.InvisibleWall);
+                tilePtr->frameX = netTile.FrameX;
+                tilePtr->frameY = netTile.FrameY;
+            }
+        }
     }
 
     private void OnWorldTileInteract(PlayerEntity plr, ref WorldTileInteract packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
     {
-        if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
+        if (plr.Phase != ConnectionPhase.Connected ||
+            packet.TileX <= 5 || packet.TileY <= 5 ||
+            packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
+
+        bool shouldSendTileSquare = false;
+
+        switch (packet.Action)
+        {
+            case 0:
+                WorldGen.KillTile(packet.TileX, packet.TileY, packet.Flags == 1);
+                if (Main.netMode == 1 && packet.Flags != 1)
+                    HitTile.ClearAllTilesAtThisLocation(packet.TileX, packet.TileY);
+                break;
+            case 1:
+            {
+                bool forced = !WorldGen.CheckTileBreakability2_ShouldTileSurvive(packet.TileX, packet.TileY);
+                if (!forced)
+                    shouldSendTileSquare = true;
+                WorldGen.PlaceTile(packet.TileX, packet.TileY, packet.Type, mute: false, forced, -1, packet.Flags);
+                break;
+            }
+            case 2:
+                WorldGen.KillWall(packet.TileX, packet.TileY, packet.Flags == 1);
+                break;
+            case 3:
+                WorldGen.PlaceWall(packet.TileX, packet.TileY, packet.Type);
+                break;
+            case 4:
+                WorldGen.KillTile(packet.TileX, packet.TileY, packet.Flags == 1, effectOnly: false, noItem: true);
+                break;
+            case 5:
+                WorldGen.PlaceWire(packet.TileX, packet.TileY);
+                break;
+            case 6:
+                WorldGen.KillWire(packet.TileX, packet.TileY);
+                break;
+            case 7:
+                WorldGen.PoundTile(packet.TileX, packet.TileY);
+                break;
+            case 8:
+                WorldGen.PlaceActuator(packet.TileX, packet.TileY);
+                break;
+            case 9:
+                WorldGen.KillActuator(packet.TileX, packet.TileY);
+                break;
+            case 10:
+                WorldGen.PlaceWire2(packet.TileX, packet.TileY);
+                break;
+            case 11:
+                WorldGen.KillWire2(packet.TileX, packet.TileY);
+                break;
+            case 12:
+                WorldGen.PlaceWire3(packet.TileX, packet.TileY);
+                break;
+            case 13:
+                WorldGen.KillWire3(packet.TileX, packet.TileY);
+                break;
+            case 14:
+                WorldGen.SlopeTile(packet.TileX, packet.TileY, packet.Type);
+                break;
+            case 15:
+                Minecart.FrameTrack(packet.TileX, packet.TileY, pound: true);
+                break;
+            case 16:
+                WorldGen.PlaceWire4(packet.TileX, packet.TileY);
+                break;
+            case 17:
+                WorldGen.KillWire4(packet.TileX, packet.TileY);
+                break;
+            case 18:
+                Wiring.SetCurrentUser(plr.Index);
+                Wiring.PokeLogicGate(packet.TileX, packet.TileY);
+                Wiring.SetCurrentUser();
+                return;
+            case 19:
+                Wiring.SetCurrentUser(plr.Index);
+                Wiring.Actuate(packet.TileX, packet.TileY);
+                Wiring.SetCurrentUser();
+                return;
+            case 20:
+                int prevType = Main.tile[packet.TileX, packet.TileY].type;
+                WorldGen.KillTile(packet.TileX, packet.TileY, packet.Flags == 1);
+                packet.Type = (short)((Main.tile[packet.TileX, packet.TileY].active() && Main.tile[packet.TileX, packet.TileY].type == prevType) ? 1 : 0);
+
+                NetMessage.TrySendData(17, -1, -1, NetworkText.Empty, packet.Action, packet.TileX, packet.TileY, packet.Type, packet.Flags);
+                return;
+            case 21:
+                WorldGen.ReplaceTile(packet.TileX, packet.TileY, (ushort)packet.Type, packet.Flags);
+                break;
+            case 22:
+                WorldGen.ReplaceWall(packet.TileX, packet.TileY, (ushort)packet.Type);
+                break;
+            case 23:
+                WorldGen.SlopeTile(packet.TileX, packet.TileY, packet.Type);
+                WorldGen.PoundTile(packet.TileX, packet.TileY);
+                break;
+        }
+
+        if (shouldSendTileSquare)
+        {
+            NetMessage.SendTileSquare(-1, packet.TileX, packet.TileY, 5);
+        }
+        else if ((packet.Action != 1 && packet.Action != 21) || !TileID.Sets.Falling[packet.Type] || Main.tile[packet.TileX, packet.TileY].active())
+        {
+            NetMessage.TrySendData(17, -1, plr.Index, NetworkText.Empty, packet.Action, packet.TileX, packet.TileY, packet.Type, packet.Flags);
+        }
     }
 
     private void OnWorldPaintWall(PlayerEntity plr, ref WorldPaintWall packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
     {
         if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
+
+        var tile = Main.tile[packet.TileX, packet.TileY];
+        if (tile == null)
+        {
+            return;
+        }
+
+        if (packet.IsCoating == 1)
+        {
+            WorldGen.paintCoatWall(packet.TileX, packet.TileY, packet.PaintType);
+        }
+        else
+        {
+            WorldGen.paintWall(packet.TileX, packet.TileY, packet.PaintType);
+        }
+
+        NetMessage.TrySendData(64, -1, plr.Index, NetworkText.Empty, packet.TileX, packet.TileY, packet.IsCoating, packet.PaintType);
     }
 
     private void OnWorldPaintTile(PlayerEntity plr, ref WorldPaintTile packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
     {
         if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
+
+        var tile = Main.tile[packet.TileX, packet.TileY];
+        if (tile == null)
+        {
+            return;
+        }
+
+        if (packet.IsCoating == 1)
+        {
+            WorldGen.paintCoatTile(packet.TileX, packet.TileY, packet.PaintType);
+        }
+        else
+        {
+            WorldGen.paintTile(packet.TileX, packet.TileY, packet.PaintType);
+        }
+
+        NetMessage.TrySendData(65, -1, plr.Index, NetworkText.Empty, packet.TileX, packet.TileY, packet.IsCoating, packet.PaintType);
     }
 
     private void OnWorldMassWireOperation(PlayerEntity plr, ref WorldMassWireOperation packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
@@ -107,35 +315,47 @@ public sealed class WorldHandler : INetworkHandler
             packet.EndX <= 5 || packet.EndY <= 5 || packet.EndX >= Main.maxTilesX - 5 || packet.EndY >= Main.maxTilesY - 5)
             return;
 
-
-        if (Main.tile[packet.TileX, packet.TileY] == null)
-        {
-            Main.tile[packet.TileX, packet.TileY] = new Tile();
-        }
-        lock (Main.tile[packet.TileX, packet.TileY])
-        {
-            Main.tile[packet.TileX, packet.TileY].liquid = b15;
-            Main.tile[packet.TileX, packet.TileY].liquidType(liquidType);
-            if (Main.netMode == 2)
-            {
-                WorldGen.SquareTileFrame(packet.TileX, packet.TileY);
-                if (b15 == 0)
-                {
-                    NetMessage.SendData(48, -1, whoAmI, null, packet.TileX, packet.TileY);
-                }
-            }
-            break;
-        }
+		WiresUI.Settings.MultiToolMode toolMode2 = WiresUI.Settings.ToolMode;
+		WiresUI.Settings.ToolMode = (WiresUI.Settings.MultiToolMode)packet.ToolMode;
+		Wiring.MassWireOperation(new(packet.StartX, packet.StartY), new(packet.EndX, packet.EndY), Main.player[plr.Index]);
+		WiresUI.Settings.ToolMode = toolMode2;
     }
 
     private void OnWorldLockSomething(PlayerEntity plr, ref WorldLockSomething packet, ReadOnlySpan<byte> rawPacket, ref bool ignore)
     {
         if (plr.Phase != ConnectionPhase.Connected || packet.TileX <= 5 || packet.TileY <= 5 || packet.TileX >= Main.maxTilesX - 5 || packet.TileY >= Main.maxTilesY - 5)
             return;
+
+        switch (packet.TargetType)
+        {
+            case 1:
+                Chest.Unlock(packet.TileX, packet.TileY);
+                break;
+            case 2:
+                WorldGen.UnlockDoor(packet.TileX, packet.TileY);
+                break;
+            case 3:
+                Chest.Lock(packet.TileX, packet.TileY);
+                break;
+            default:
+                return;
+        }
+
+        NetMessage.TrySendData(52, -1, plr.Index, NetworkText.Empty, 0, packet.TargetType, packet.TileX, packet.TileY);
+        NetMessage.SendTileSquare(-1, packet.TileX, packet.TileY, 2);
     }
 
     public void Unload()
     {
-
+        NetworkManager.SetMainHandler<WorldLockSomething>(null);
+        NetworkManager.SetMainHandler<WorldMassWireOperation>(null);
+        NetworkManager.SetMainHandler<WorldPaintTile>(null);
+        NetworkManager.SetMainHandler<WorldPaintWall>(null);
+        NetworkManager.SetMainHandler<WorldTileInteract>(null);
+        NetworkManager.SetMainHandler<WorldTileRectangle>(null);
+        NetworkManager.SetMainHandler<WorldToggleGemLock>(null);
+        NetworkManager.SetMainHandler<WorldWiringHitSwitch>(null);
+        NetworkManager.SetMainHandler<WorldAddLiquid>(null);
+        NetworkManager.SetMainHandler<WorldDoorInteract>(null);
     }
 }
