c4cbdd1c279a315fa18b9c86d47a81b92c726891
dsaf <ghostyn678+git@gmail.com>
2025-06-13 17:31:06 -0300

    feat(PlayerUtils): localized text support

--- src/Server/Entities/Players/PlayerUtils.cs
+++ src/Server/Entities/Players/PlayerUtils.cs
@@ -4,18 +4,20 @@ namespace Amethyst.Server.Entities.Players;
 
 public static class PlayerUtils
 {
-    public static void BroadcastText(string text, byte r, byte g, byte b)
+    public static void BroadcastText(string text, byte r, byte g, byte b, bool localized = false)
     {
         foreach (PlayerEntity plr in EntityTrackers.Players)
         {
             if (plr.Active)
             {
-                plr.SendText(text, r, g, b);
+                string msg = localized && plr.User != null ? Localization.Get(text, plr.User.Messages.Language) : text;
+
+                plr.SendText(msg, r, g, b);
             }
         }
     }
 
-    public static void BroadcastText(string text, NetColor color) => BroadcastText(text, color.R, color.G, color.B);
+    public static void BroadcastText(string text, NetColor color, bool localized = false) => BroadcastText(text, color.R, color.G, color.B, localized);
 
     public static void BroadcastPacketBytes(byte[] packetBytes, int ignore = -1)
     {
