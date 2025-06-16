c57ea305af9053c33c026e492854b4cf518fb0c6
dsaf <ghostyn678+git@gmail.com>
2025-06-16 01:53:11 -0300

    fix(CommandSyntaxAttribute): Throw if no syntax entry is added

--- src/Systems/Commands/Dynamic/Attributes/CommandSyntaxAttribute.cs
+++ src/Systems/Commands/Dynamic/Attributes/CommandSyntaxAttribute.cs
@@ -4,5 +4,7 @@ namespace Amethyst.Systems.Commands.Dynamic.Attributes;
 public sealed class CommandSyntaxAttribute(string culture, params string[] syntax) : Attribute
 {
     public string Culture { get; } = culture;
-    public string[] Syntax { get; } = syntax;
+    public string[] Syntax { get; } = syntax.Length > 0
+        ? syntax
+        : throw new ArgumentException("CommandSyntaxAttribute requires at least one syntax entry.", nameof(syntax));
 }
